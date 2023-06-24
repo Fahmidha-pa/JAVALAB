e9ffb18e0626766385ae9bbbba44d8ac0416c36f
Fahmidha-pa <116252309+Fahmidha-pa@users.noreply.github.com>
2023-06-24 12:32:41 +0530

    linkedlist

---
+++ pgrm22 linkedlist.java/LinkedLists.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class LinkedLists
+{
+public static void main(String args[]){
+LinkedList<String>ll=new LinkedList<String>();
+ll.add("a");
+ll.add("b");
+ll.add("c");
+ll.add("d");
+ll.add("e");
+ll.add("f");
+System.out.println("current conetents of ||:" +ll);
+ll.clear();
+System.out.println("Contents of || after deletion:"+ll);
+}
+}
